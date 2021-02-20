4c7d27a2af96cc6a2b3c0396a0cbb30d5093667e
Antony <antonydellavecchia@gmail.com>
2021-02-20 20:27:17 +0100

    create engine fucntion

--- one_relator_curvature/database.py
+++ one_relator_curvature/database.py
@@ -1,15 +1,24 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
 from results import Base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
+from pathlib import Path
 
-engine = create_engine("sqlite:////home/antony/one_relator_curvature/examples.db")
+
 Session = sessionmaker(bind=engine)
 
 
-def recreate_database():
+def create_engine_from_path(database_path: Path) -> Engine:
+    """Returns sqlalchemy engine at given path using sqlite"""
+    return create_engine(f"sqlite:///{database_path}")
+
+
+
+def recreate_database(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
 
 @contextmanager
 def session_scope():
